7d80c1739e8c403040f72143e3ff017efcc76225
Liwsaki Live <natthaphon@nipa.cloud>
2018-11-03 15:47:45 +0700

    first commit

---
+++ openstack-queen-aio.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e -x -u
+
+apt update
+apt -y dist-upgrade
+apt install -y git vim python unzip iotop htop iftop
+
+# # List all existing tags.
+git tag -l /opt/openstack-ansible
+
+git checkout stable/queens /opt/openstack-ansible
+git describe --abbrev=0 --tags /opt/openstack-ansible
+git checkout 17.1.3 /opt/openstack-ansible
