6779434ab9a655ebd9abc28b01c29be719d7fe2d
Liwsaki Live <natthaphon@nipa.cloud>
2018-11-03 16:09:08 +0700

    first commit

--- openstack-queen-aio.sh
+++ openstack-queen-aio.sh
@@ -9,9 +9,20 @@ apt install -y git vim python unzip iotop htop iftop
 git clone https://git.openstack.org/openstack/openstack-ansible \
     /opt/openstack-ansible
 
-# # List all existing tags.
-git tag -l /opt/openstack-ansible
+git clone -b 17.1.3 https://github.com/openstack/openstack-ansible /opt/openstack-ansible
 
-git checkout stable/queens /opt/openstack-ansible
-git describe --abbrev=0 --tags /opt/openstack-ansible
-git checkout 17.1.3 /opt/openstack-ansible
+cd /opt/openstack-ansible/
+
+scripts/bootstrap-ansible.sh
+scripts/bootstrap-aio.sh
+
+
+cd /opt/openstack-ansible/
+cp etc/openstack_deploy/conf.d/{aodh,gnocchi,ceilometer}.yml.aio /etc/openstack_deploy/conf.d/
+for f in $(ls -1 /etc/openstack_deploy/conf.d/*.aio); do mv -v ${f} ${f%.*}; done
+
+cd /opt/openstack-ansible/playbooks
+
+openstack-ansible setup-hosts.yml
+openstack-ansible setup-infrastructure.yml
+openstack-ansible setup-openstack.yml
