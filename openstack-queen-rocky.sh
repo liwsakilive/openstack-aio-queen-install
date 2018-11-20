d04b5b5890eff49f12ba9b141b14b92fb6f8b2a5
Liwsaki Live <natthaphon@nipa.cloud>
2018-11-20 13:42:56 +0700

    rocky

--- openstack-queen-rocky.sh
+++ openstack-queen-rocky.sh
@@ -1,20 +1,22 @@
-#!/usr/bin/env bash
+#!/usr/bin/
 
 set -e -x -u
 
-apt update
-apt -y dist-upgrade
+apt-get update
+apt-get dist-upgrade
 apt install -y git vim python unzip iotop htop iftop
 
-git clone -b 17.1.3 https://github.com/openstack/openstack-ansible /opt/openstack-ansible
+git clone -b 18.0.0 https://github.com/openstack/openstack-ansible /opt/openstack-ansible
 
 cd /opt/openstack-ansible/
 
 scripts/bootstrap-ansible.sh
+
 scripts/bootstrap-aio.sh
 
 
 cd /opt/openstack-ansible/
+
 cp etc/openstack_deploy/conf.d/{aodh,gnocchi,ceilometer}.yml.aio /etc/openstack_deploy/conf.d/
 for f in $(ls -1 /etc/openstack_deploy/conf.d/*.aio); do mv -v ${f} ${f%.*}; done
 
