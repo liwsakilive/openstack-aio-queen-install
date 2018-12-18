2cd2165d2fe44e8d3fee0878813fafa4e231c3e9
Liwsaki Live <natthaphon@nipa.cloud>
2018-12-18 10:59:48 +0700

    set -y

--- openstack-rocky-aio.sh
+++ openstack-rocky-aio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt-get update
-apt-get dist-upgrade
+apt-get update -y
+apt-get dist-upgrade -y
 apt-get install -y git vim python unzip iotop htop iftop
 
 git clone -b 18.0.0 https://github.com/openstack/openstack-ansible /opt/openstack-ansible
