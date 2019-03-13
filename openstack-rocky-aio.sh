ae52e01b9de9eae70332d54ad0a2f6b798d01a19
Liwsaki Live <natthaphon.phn@spulive.net>
2019-03-13 13:24:56 +0700

    Update openstack-rocky-aio.sh

--- openstack-rocky-aio.sh
+++ openstack-rocky-aio.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -e -x -u
 
 apt-get update -y
 apt-get dist-upgrade -y
-apt-get install -y git vim python virtualenv  unzip iotop htop iftop
+apt-get install -y git vim python unzip iotop htop iftop
 
 git clone -b 18.0.0 https://github.com/openstack/openstack-ansible /opt/openstack-ansible
 
